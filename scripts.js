d4da14a2ab1587afb40c149178a1425458225968
Olivia Rudd <oliviadavidsonrudd@gmail.com>
2022-01-11 20:49:47 -0500

    Add scripts.js and began using js to handle numbers.

---
+++ scripts.js
@@ -0,0 +1,12 @@
+
+ac();
+function ac() {
+    let baseAc = 23;
+    let shieldAc = baseAc + 4;
+    document.getElementById("shieldAc").innerHTML = shieldAc;
+    document.getElementById("baseAc").innerHTML = baseAc;
+    
+}
+
+
+
