86f1a32adebe371450692ef6e109a1a285d2312e
Olivia Rudd <oliviadavidsonrudd@gmail.com>
2022-01-21 17:00:44 -0500

    Add stat save values Also fixed issue where stat or mod values of 0 caused them to not be displayed.

--- scripts.js
+++ scripts.js
@@ -1,7 +1,11 @@
 
 // Global constants:
+const profBonus = 6;
 const stats = ["str", "dex", "con", "int", "wis", "cha"];
 const statScoreArray = [14, 22, 14, 11, 12, 12];
+const statSaveMultiplier = [1, 0, 1, 0, 0, 0];
+const skills = ["acro", "anim", "arca", "athl", "dece", "hist", "insi", "inti", "inve", "medi", "natu", "perc", "perf", "pers", "reli", "slei", "stea", "surv"];
+
 
 // AC
 ac();
@@ -26,17 +30,40 @@ function statMods() {
     const statModArray = statScoreArray.map(scoreToMod);
 
     for (const stat of stats) {
-        let className = `${stat}ModElement`;
+        let className = `${stat}Mod`; 
         let statIndex = stats.indexOf(stat);
         let classElements = document.getElementsByClassName(className);
         for (classElement of classElements) {
             classElement.textContent = plusSigns(statModArray[statIndex]);
         }
     }
-}
+    //create new array of stat save values
+
+    function modToSave(statMod) {
+        return statMod + (profBonus * (statSaveMultiplier[statModArray.indexOf(statMod)]));
+    }
+     
+    const statSaveArray = statModArray.map(modToSave);
+    console.log(statSaveArray)
+
+    // apply stat save values to stat save elements
+    for (const stat of stats) {
+        let className = `${stat}Save`; 
+        let statIndex = stats.indexOf(stat);
+        let classElements = document.getElementsByClassName(className);
+        for (classElement of classElements) {
+            classElement.textContent = plusSigns(statSaveArray[statIndex]);
+        }
+    }
+
+    }
+
 
 function plusSigns(number) {
     if (number >0) { 
        return number = "+" + number;
+    }
+    else {
+        return number;
     }
 }
